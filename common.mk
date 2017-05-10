a6dd9b92c99cc1ca04ad46ebf081bdd1f302f9b2
Simon Shields <simon@lineageos.org>
2017-05-10 23:04:00 +1000

    smdk4412: ship Jelly
    Change-Id: I2916fa4be74ab941f45bd23907ee2488336eae1a

--- common.mk
+++ common.mk
@@ -63,7 +63,6 @@ PRODUCT_PACKAGES := \
     audio.r_submix.default \
     audio.usb.default \
     com.android.future.usb.accessory \
-    Gello \
     gralloc.exynos4 \
     hwcomposer.exynos4 \
     libfimc \
